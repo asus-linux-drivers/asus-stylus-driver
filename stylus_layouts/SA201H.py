fc589d37e429b6ff75f1b3322c521b528bacb3ed
Lukáš Drahník <ldrahnik@gmail.com>
2023-07-02 13:33:47 +0200

    More described layout SA201H which button is first and which second from tip of stylus

--- stylus_layouts/SA201H.py
+++ stylus_layouts/SA201H.py
@@ -2,12 +2,12 @@ from libevdev import EV_KEY
 
 keys = [ 
     [
-        EV_KEY.BTN_TOOL_RUBBER, # listen
+        EV_KEY.BTN_TOOL_RUBBER, # listen first button from the tip
         589827, # MSC_SCAN code of BTN_MIDDLE
         EV_KEY.BTN_MIDDLE # pressed & unpressed key
     ],
     [
-        EV_KEY.BTN_STYLUS, # listen
+        EV_KEY.BTN_STYLUS, # listen second button from the tip
         589826, # MSC_SCAN code of BTN_RIGHT
         EV_KEY.BTN_RIGHT # pressed & unpressed key
     ]
